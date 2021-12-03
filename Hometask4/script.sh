550682de2bd312ba32f57fb8eec3d0fa1d76273d
Mar1kP <pigruhm@gmail.com>
2021-12-03 13:50:37 +0200

    task

--- Hometask4/script.sh
+++ Hometask4/script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo useradd adminuser
-sudo usermod -aG sudo adminuser
-sudo useradd poweruser
-sudo bash -c 'echo "poweruser ALL=(ALL:ALL) /sbin/iptables" >> /etc/sudoers'
+useradd adminuser
+echo 'adminuser:pass' | sudo chpasswd
+groupadd admin
+usermod -aG admin adminuser 
+
+
+useradd poweruser
+bash -c echo "poweruser ALL=(ALL:ALL) /sbin/iptables" >> /etc/sudoers
 usermod -aG adminuser poweruser
-sudo find /home/ -perm /640
+find / -type d -perm 4000 -exec ls -ld {} \; >> /var/suid_files.txt
